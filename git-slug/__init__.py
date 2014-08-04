e2135bc0bc44d8d7fe7c5dfcefa9802c06db1fb1
Aaron Iles <aaron.iles@gmail.com>
2014-08-04 19:50:42 +1000

    New project built from touchstone template
    Set up git-slug project using the touchstone project template
    https://github.com/aliles/touchstone

---
+++ git-slug/__init__.py
@@ -0,0 +1,2 @@
+from __future__ import absolute_import, division, print_function
+from git-slug.version import __version__
---
+++ tests/test_git-slug.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function
+
+try:
+    # python 2.x
+    import unittest2 as unittest
+except ImportError:
+    # python 3.x
+    import unittest
+
+import doctest
+
+import git-slug
+
+
+class TestGitSlug(unittest.TestCase):
+
+    def test_has_version(self):
+        self.assertTrue(git-slug.__version__)
+
+    def test_readme(self):
+        doctest.testfile('../README.rst')
